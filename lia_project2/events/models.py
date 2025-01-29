0a94f810abd580b8ef263edd6448eb1402377677
ffcweb <ffchen.ca@gmail.com>
2025-01-29 14:25:10 -0500

    added class Event

--- lia_project2/events/models.py
+++ lia_project2/events/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    max_attendees = models.PositiveIntegerField()
+    location = models.CharField(max_length=250)
+    latitude = models.FloatField(null=True, blank=True)  # Store latitude
+    longitude = models.FloatField(null=True, blank=True)  # Store longitude
+    image = models.ImageField(upload_to='events/', blank=True, null=True)
+    category = models.CharField(max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
