54bf2b9dfd1077db54e8fdea9bbbb60532230445
Gil <mobg411@gmail.com>
2025-01-29 15:55:26 -0500

    add login logout views.py

--- lia_project2/accounts/views.py
+++ lia_project2/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from accounts.models import User
 
 def register(request):
@@ -44,3 +44,22 @@ def register(request):
             return render(request, "accounts/register.html", {"error": e.message})
 
     return render(request, "accounts/register.html")
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect("home")
+        else:
+            return render(request, "accounts/login.html", {"error": "Invalid username or password."})
+
+    return render(request, "accounts/login.html")
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
