1ba249d5de14568deed11e34832af8773682bd8f
ffcweb <ffchen.ca@gmail.com>
2025-01-29 15:23:24 -0500

    Created classes in models.py file, closes #20

--- lia_project2/events/models.py
+++ lia_project2/events/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
+    
     
 class Event(models.Model):
     title = models.CharField(max_length=200)
