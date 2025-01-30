55566577ed06b960d889a35e5a60febd2ac950cf
Gil <mobg411@gmail.com>
2025-01-29 21:45:35 -0500

    Modified list_filter in admin.py

--- lia_project2/accounts/admin.py
+++ lia_project2/accounts/admin.py
@@ -4,8 +4,8 @@ from .models import User
 
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ("username", "email", "date_joined", "is_staff", "is_active", "is_admin")
-    list_filter = ("is_staff", "is_active", "is_admin")
+    list_display = ("username", "email", "date_joined", "is_admin")
+    list_filter = ("is_admin",)
     search_fields = ("username", "email")
     ordering = ("username",)
     
@@ -14,11 +14,11 @@ class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'email', 'password')}),
         ('Profile Info', {'fields': ('bio', 'profile_picture')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_admin')}),
+        ('Permissions', {'fields': ('is_admin',)}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
 
-    filter_horizontal = ()  
+    filter_horizontal = ()
 
     exclude = ("groups", "user_permissions")
 
