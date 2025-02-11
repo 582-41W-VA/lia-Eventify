c4e915b89a30c6e9b7937b6ecde7f1eea7821f27
Gil <mobg411@gmail.com>
2025-02-11 14:05:12 -0500

    Remove duplicates url from the same namespace accounts

--- lia_project2/eventify/urls.py
+++ lia_project2/eventify/urls.py
@@ -22,8 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("accounts.urls")),
-    path('accounts/', include(('accounts.urls', 'accounts'))),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
     path("events/", include("events.urls")),  
     path("", homepage, name="homepage"),
 ]
