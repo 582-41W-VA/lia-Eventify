7f1a6268fe919cefac02f36b2368d3e9be8b32fa
ffcweb <ffchen.ca@gmail.com>
2025-02-13 18:27:01 -0500

    added def  admin_statistics

--- lia_project2/events/views_admin.py
+++ lia_project2/events/views_admin.py
@@ -5,3 +5,8 @@ from django.db.models import Count
 from .models import Event
 from django.contrib.auth import get_user_model
 
+
+@staff_member_required
+def admin_statistics(request):
+  
+    return render(request, 'events/admin/statistics.html', context)
