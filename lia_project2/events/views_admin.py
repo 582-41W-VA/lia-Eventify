4a90d6d0e8f1c6c231075e831170a1af158c98f7
ffcweb <ffchen.ca@gmail.com>
2025-02-13 18:26:28 -0500

    added imports

--- lia_project2/events/views_admin.py
+++ lia_project2/events/views_admin.py
@@ -1,1 +1,7 @@
 # events/views_admin.py
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render
+from django.db.models import Count
+from .models import Event
+from django.contrib.auth import get_user_model
+
